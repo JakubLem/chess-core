0eda2d9eece1cef4730994b5dffd4f4cefe85ac2
Jakub <jakub.lemiesiewicz@gmail.com>
2022-10-28 20:49:14 +0200

    try to implement sio

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,7 +1,19 @@
+# from api.views import sio
+import socketio
+
 import os
 
 from django.core.wsgi import get_wsgi_application
+from api.sio import sio
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+dj_application  = get_wsgi_application()
+application = socketio.WSGIApp(sio, dj_application)
 
-application = get_wsgi_application()
+####################################################################################
+
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
+
+server = pywsgi.WSGIServer(("", 8000), application, handler_class=WebSocketHandler)
+server.serve_forever()
