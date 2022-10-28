33ae158a19dd11746458b1908a78e4fe3a34a40d
Jakub <jakub.lemiesiewicz@gmail.com>
2022-10-28 21:12:21 +0200

    try to fix heroku

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,17 +1,26 @@
-# from api.views import sio
-import socketio
+# socket io
+# import socketio
+# 
+# import os
+# 
+# from django.core.wsgi import get_wsgi_application
+# from api.sio import sio
+# 
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+# dj_application  = get_wsgi_application()
+# application = socketio.WSGIApp(sio, dj_application)
+# 
+# from gevent import pywsgi
+# from geventwebsocket.handler import WebSocketHandler
+# 
+# server = pywsgi.WSGIServer(("", 8000), application, handler_class=WebSocketHandler)
+# server.serve_forever()
+
 
 import os
 
 from django.core.wsgi import get_wsgi_application
-from api.sio import sio
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-dj_application  = get_wsgi_application()
-application = socketio.WSGIApp(sio, dj_application)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-from gevent import pywsgi
-from geventwebsocket.handler import WebSocketHandler
-
-server = pywsgi.WSGIServer(("", 8000), application, handler_class=WebSocketHandler)
-server.serve_forever()
+application = get_wsgi_application()
