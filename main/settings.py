09e8e644d9829b33c66b4bc670b515d285f16606
Jakub <jakub.lemiesiewicz@gmail.com>
2022-11-02 19:15:04 +0100

    CHC-6 try to install channels

--- main/settings.py
+++ main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["127.0.0.1", "lemis-chess-core.herokuapp.com"]
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
