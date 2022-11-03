30450867c87e8f4a26c27251882be4eb84aeb2ee
Jakub <jakub.lemiesiewicz@gmail.com>
2022-11-03 23:21:15 +0100

    try to fix re_path

--- main/routing.py
+++ main/routing.py
@@ -19,11 +19,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 from api.consumers import TextRoomConsumer
 websocket_urlpatterns = [
-    re_path(r'^ws/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
+    re_path(r'ws/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
 ]
 # the websocket will open at 127.0.0.1:8000/ws/<room_name>
-
 application = ProtocolTypeRouter({
+    # "http": get_asgi_application(),
     'websocket':
         URLRouter(
             websocket_urlpatterns
